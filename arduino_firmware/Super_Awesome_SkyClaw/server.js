c7ac58209503185270a57151679afd96a28a27d6
Jay Beavers <jay@hikinghomeschoolers.org>
2013-12-07 16:29:31 -0500

    Final tunings and integration

--- arduino_firmware/Super_Awesome_SkyClaw/server.js
+++ arduino_firmware/Super_Awesome_SkyClaw/server.js
@@ -24,13 +24,16 @@ http.createServer(function(req, res) {
 	}
 
 	res.writeHead(200, {'Content-Type': 'text/html'});
-  if (fileName == 'open' || fileName == "close") {
+  if (fileName == 'v' || fileName == "c") {
+  	fileName = fileName == 'v' ? 'open' : 'close';
     request.post(
       'https://api.spark.io/v1/devices/53ff6a065067544855371087/claw',
       { form: { access_token: '560699bafcadc1c3bc6cb697a992a14f7fb9af09', argggh: fileName } },
       function (error, response, body) {
           if (!error && response.statusCode == 200) {
               console.log(fileName + 'ed the claw!');
+          } else {
+          	console.log(error, response, body);
           }
       }
   );
